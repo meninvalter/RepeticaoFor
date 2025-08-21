4d70d68e926a5cab75343bd8cad259b1158511b9
menin <meninvalterpedro@gmail.com>
2025-08-21 16:50:17 +0100

    While

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -11,9 +11,12 @@ public class Main {
 
         Scanner sc = new Scanner(System.in);
         int x = sc.nextInt();
+        double soma = 0;
         while (x!=0){
+            soma += x;
             x = sc.nextInt();
         }
+        System.out.println(soma);
 
 //        int N = sc.nextInt();
 //        int soma =0;
